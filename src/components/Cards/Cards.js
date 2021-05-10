47802dd4b32c8d8461e4b88bc2c8b8fe03cd0731
Carolina Maffia <carolinamaffia.m@gmail.com>
2021-05-10 17:32:08 -0300

    final changes

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,11 +1,86 @@
+//styles
 import './cards.scss';
-
+//Components
 import Stories from '../Stories/Stories';
+import Card from '../Card/Card';
 
 const Cards = () => {
+    const commentsOne = [
+        {
+          user: "raffagrassetti",
+          text: "Woah dude, this is awesome! 🔥",
+          id: 1,
+        },
+        {
+          user: "therealadamsavage",
+          text: "Like!",
+          id: 2,
+        },
+        {
+          user: "mapvault",
+          text: "Niceeeee!",
+          id: 3,
+        },
+      ];
+    
+      const commentsTwo = [
+        {
+          user: "mapvault",
+          text: "Amazing content, keep it up!",
+          id: 4,
+        },
+      ];
+    
+      const commentsThree = [
+        {
+          user: "dadatlacak",
+          text: "Love this!",
+          id: 5,
+        },
+      ];
+    
     return (
         <div className="cards">
             <Stories />
+            <Card 
+                accountName="burningxhearts" 
+                storyBorder={true} 
+                image="https://picsum.photos/800/900" 
+                comments={commentsOne} 
+                likedByText="datita" 
+                likedByNumber="89" 
+                hours={18} 
+            />
+
+            <Card 
+                accountName="burningxhearts" 
+                storyBorder={true} 
+                image="https://picsum.photos/800/600" 
+                comments={commentsTwo} 
+                likedByText="datita" 
+                likedByNumber="89" 
+                hours={18} 
+            />
+
+            <Card 
+                accountName="burningxhearts" 
+                storyBorder={true} 
+                image="https://picsum.photos/800" 
+                comments={commentsThree} 
+                likedByText="datita" 
+                likedByNumber="89" 
+                hours={18} 
+            />
+
+            <Card 
+                accountName="burningxhearts" 
+                storyBorder={true} 
+                image="https://picsum.photos/800/1000" 
+                comments={commentsOne} 
+                likedByText="datita" 
+                likedByNumber="89" 
+                hours={18} 
+            />
         </div>
     )
 }
